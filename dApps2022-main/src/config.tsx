33beb8b9ea755aaf66b52b6aa23b4c302e6dcaec
Héctor Avelar <103058629+avelarh368@users.noreply.github.com>
2022-10-04 14:35:46 -0500

    Add files via upload
    Se añadio la pagina settings (esta con etiquetas de HTML y con diseño de bootstrap).

---
+++ dApps2022-main/src/config.tsx
@@ -0,0 +1,11 @@
+/*
+ * General file to be imported to every other module in the project, contains
+ * a path to the home directory as well as the public key for JSONWebTokens and
+ * OAuth.
+*/ 
+type CONFIG = {
+  HOME: "/src/",
+  TOKENIZER: "D1h1B1#M#tI8cSoK"
+}
+
+export type { CONFIG };
